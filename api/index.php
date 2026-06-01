e57208b1a453aa6c2baeb476de3b14e05575e67e
Alhamid Adriansyah <alhamidadriansyah11@gmail.com>
2026-06-01 12:51:01 +0700

    deployment

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
-// Meneruskan request dari serverless function Vercel ke public/index.php Laravel
-require __DIR__ . '/../public/index.php';
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
+
+$storagePath = $app->storagePath();
+
+foreach (['app', 'framework/views', 'framework/cache', 'framework/sessions', 'logs'] as $dir) {
+    if (!is_dir("{$storagePath}/{$dir}")) {
+        mkdir("{$storagePath}/{$dir}", 0777, true);
+    }
+}
+
+$app->handleRequest(Illuminate\Http\Request::capture());
