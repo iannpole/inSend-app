a173bea0f73a1616ce728e93ce85b1ef14efdda5
Alhamid Adriansyah <alhamidadriansyah11@gmail.com>
2026-06-01 13:09:44 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(function (\Illuminate\Http\Request $request, \Throwable $e) {
+            return true;
+        });
     })->create();
 
